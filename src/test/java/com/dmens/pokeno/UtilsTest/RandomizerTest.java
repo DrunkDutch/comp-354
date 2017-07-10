1c168c0568013aa3d83f640b3f6d1fabe919fcf8
JRF27 <jrfiess@gmail.com>
2017-07-10 19:41:44 -0400

    Now the test is effectively done 10 times and the average is taken - results are pretty close so I made the range smaller.

--- src/test/java/com/dmens/pokeno/UtilsTest/RandomizerTest.java
+++ src/test/java/com/dmens/pokeno/UtilsTest/RandomizerTest.java
@@ -12,10 +12,10 @@ import com.dmens.pokeno.utils.Randomizer;
 public class RandomizerTest {
 	
 	static int mDesiredResults = 50;
-	static double mThreshold = 0.10;
+	static double mThreshold = 5;
 	static int mFlips = mDesiredResults * 2;
 	
-	//TODO static int mFlipRounds = 10;
+	static int mFlipRounds = 10;
 
 	@Test
 	public void Randomizer50Test() {
@@ -24,27 +24,37 @@ public class RandomizerTest {
 		rando = Randomizer.Instance();
 		Assert.assertNotEquals(rando, null);
 		
-		int headsCount = 0;
-		int tailsCount = 0;
 		
-		//TODO? loop this again and take an average?!
+		float headsAvg = 0;
+		float tailsAvg = 0;
 		
-		for(int i = 0; i < mFlips; i++)
+		for(int j = 0; j < mFlipRounds; j++)
 		{
-			if(rando.getFiftyPercentChance())
-				headsCount++;
-			else
-				tailsCount++;
+			int headsCount = 0;
+			int tailsCount = 0;
+			
+			for(int i = 0; i < mFlips; i++)
+			{
+				if(rando.getFiftyPercentChance())
+					headsCount++;
+				else
+					tailsCount++;
+			}
+			headsAvg += headsCount;
+			tailsAvg += tailsCount;
+			
+			// Check that there's nothing savage...
+			Assert.assertEquals(headsCount + tailsCount , mFlips);
 		}
 		
-		// Check that there's nothing savage...
-		Assert.assertEquals(headsCount + tailsCount , mFlips);
+		headsAvg /= mFlipRounds;
+		tailsAvg /= mFlipRounds;
 		
-		// acceptable range is within 10 of desired results
-		System.out.println("RandomizerTest-Heads: " + headsCount);
-		System.out.println("RandomizerTest-Tails: " + tailsCount);
-		Assert.assertTrue((mDesiredResults - (int) (mFlips * mThreshold )) <= tailsCount && tailsCount <= (mDesiredResults + (int) (mFlips * mThreshold )));
-		Assert.assertTrue((mDesiredResults - (int) (mFlips * mThreshold )) <= headsCount && headsCount <= (mDesiredResults + (int) (mFlips * mThreshold )));
+		
+		// Acceptable range is within 5 of desired results
+		System.out.println("RandomizerTest-HeadsAvg: " + headsAvg);
+		System.out.println("RandomizerTest-TailsAvg: " + tailsAvg);
+		Assert.assertTrue((mDesiredResults - mThreshold) <= headsAvg && headsAvg <= (mDesiredResults + mThreshold));
+		Assert.assertTrue((mDesiredResults - mThreshold) <= tailsAvg && tailsAvg <= (mDesiredResults + mThreshold));
 	}
-
 }
