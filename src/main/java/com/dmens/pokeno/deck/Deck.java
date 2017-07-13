e2ff515d2042529c31006c9b0dfee7f1881f4331
Spun-G <spongey129@hotmail.com>
2017-07-12 21:31:15 -0400

    Added checks that evolved Pokemon cards are only included in decks containing their base Pokemon + each Pokemon card has at least one ability that can be played (energy costs can be met with deck build)

--- src/main/java/com/dmens/pokeno/deck/Deck.java
+++ src/main/java/com/dmens/pokeno/deck/Deck.java
@@ -3,8 +3,14 @@ package com.dmens.pokeno.deck;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import com.dmens.pokeno.ability.AbilityCost;
 import com.dmens.pokeno.card.Card;
+import com.dmens.pokeno.card.EnergyCard;
+import com.dmens.pokeno.card.EnergyTypes;
 import com.dmens.pokeno.card.Pokemon;
+import java.util.Hashtable;
+import java.util.HashSet;
+import java.util.Stack;
 
 public class Deck extends CardContainer {
 	
@@ -36,7 +42,7 @@ public class Deck extends CardContainer {
 		// deck should have exactly 60 cards
 		if(size() != 60)
 			return false;
-		// deck should have at least one pokemon card
+		// deck should have at least one base pokemon card
 		if(!deckHasPokemonCard()) 
 			return false;
 		if(deckHasMoreThanFourNoneEnergyCard())
@@ -49,7 +55,7 @@ public class Deck extends CardContainer {
 	private boolean deckHasPokemonCard() {
 		for(int i = 0; i < size(); ++i) {
 			Card card = cards.get(i);
-			if(card instanceof Pokemon)
+			if(card instanceof Pokemon && !((Pokemon)card).isEvolvedCategory())
 				return true;
 		}
 		return false;
@@ -62,10 +68,80 @@ public class Deck extends CardContainer {
 	}
 	
 	private boolean allCardsArePlayableInDeck() {
-		
-		// TODO
-		return true;
+                Stack<Pokemon> evolvedPokemon = new Stack<Pokemon>();
+                HashSet<Pokemon> pokemonInDeck = new HashSet<Pokemon>();
+                HashSet<String> basePokemonInDeckNames = new HashSet<String>();
+                Hashtable<EnergyTypes,Integer> energyCards = new Hashtable<EnergyTypes,Integer>();
+                
+                // iterate through the deck only once, then do each validation check
+		for(int i = 0; i < size(); ++i) {
+			Card card = cards.get(i);
+                        if(card instanceof Pokemon)
+                        {
+                            pokemonInDeck.add((Pokemon)card);
+                            
+                            if(((Pokemon)card).isEvolvedCategory())
+                                evolvedPokemon.add((Pokemon)card);
+                            else
+                                basePokemonInDeckNames.add(((Pokemon)card).getName().toLowerCase());
+                        }
+                        else if(card instanceof EnergyCard) {
+                            EnergyTypes energyType = ((EnergyCard)card).getCategory();
+                            Integer cardsOfSameType = energyCards.put(energyType, 1);
+                            if(cardsOfSameType != null)
+                                energyCards.put(energyType,cardsOfSameType.intValue()+1);
+                        }
+		}
+                if(basePokemonInDeckNames.isEmpty())
+                    return false;
+                
+                return basePokemonExistsForEachEvolvedPokemonInDeck(evolvedPokemon,basePokemonInDeckNames)
+                        && eachPokemonHasAPlayableAbility(pokemonInDeck,energyCards);
 	}
+        
+        private boolean basePokemonExistsForEachEvolvedPokemonInDeck(Stack<Pokemon> evolvedPokemon, HashSet<String> basePokemonInDeckNames) {
+            // check each evolved Pokemon's base Pokemon is in the deck at least once
+                while(!evolvedPokemon.isEmpty()) {
+                    Pokemon evolution = evolvedPokemon.pop();
+                    
+                    if(!basePokemonInDeckNames.contains(evolution.getBasePokemonName().toLowerCase()))
+                        return false; // evolved Pokemon was included in a deck not containing the base Pokemon
+                }
+                
+                return true;
+        }
+        
+        private boolean eachPokemonHasAPlayableAbility(HashSet<Pokemon> pokemonInDeck,Hashtable<EnergyTypes,Integer> energyCards) {
+            // check there are enough energy cards for each Pokemon to potentially use an ability
+                for(Pokemon pokemon : pokemonInDeck) {
+                    ArrayList<AbilityCost> abilityCosts = pokemon.getAbilitiesAndCost();
+                    
+                    boolean foundPlayableAbility = false;
+                    int abilityIndex = 0;
+                    // try each ability until we find one that has its required energy in the deck
+                    do {
+                        boolean abilityIsAffordable = true;
+                        
+                        AbilityCost abilityCost = abilityCosts.get(abilityIndex);
+                        
+                        // verify each energy cost for this ability can be satisfied by cards in the deck
+                        for(EnergyTypes energyType : abilityCost.getCosts().keySet()) {
+                            if(abilityCost.getCosts().get(energyType) > energyCards.get(energyType)) {
+                                // cost is greater than the number of this energy type's cards included in the deck
+                                abilityIsAffordable = false;
+                                break;
+                            }
+                        }
+                        foundPlayableAbility = abilityIsAffordable;
+                        // try next ability if current ability is too expensive for this deck
+                    } while(!foundPlayableAbility && ++abilityIndex < abilityCosts.size());
+                    
+                    if(!foundPlayableAbility) // this Pokemon cannot be played with this deck build
+                        return false;
+                }
+                
+                return true;
+        }
 	
 	public void shuffle(){
 		Collections.shuffle(cards);
