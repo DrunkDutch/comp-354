a2c530891cb9a7c646f4070dd2e28a8c94d64b41
Jumper706 <jarwen706@hotmail.com>
2017-07-31 11:17:27 -0400

    Started regression 121

---
+++ src/test/java/com/dmens/pokeno/regression/Regression121.java
@@ -0,0 +1,7 @@
+package com.dmens.pokeno.regression;
+
+public class Regression121
+{
+	//use some stubs to make the AI just draw a bunch of reward cards (maybe just call take reward directly)
+	
+}
