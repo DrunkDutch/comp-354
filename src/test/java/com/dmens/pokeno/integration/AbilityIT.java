a0daffb8824ec444d39ab5e1ff14ed6150b307f0
Devin Mens <devin.mens@gmail.com>
2017-07-31 01:10:01 -0400

    #117 Merged master changes and separated modified tests into the integration package

--- src/test/java/com/dmens/pokeno/integration/AbilityIT.java
+++ src/test/java/com/dmens/pokeno/integration/AbilityIT.java
@@ -8,9 +8,11 @@ import com.dmens.pokeno.database.AbilitiesDatabase;
 import com.dmens.pokeno.database.CardsDatabase;
 import com.dmens.pokeno.deck.Deck;
 import com.dmens.pokeno.player.Player;
+import com.dmens.pokeno.services.handlers.TargetServiceHandler;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -48,7 +50,7 @@ public class AbilityIT {
         Deck deck = new Deck();
         deck.addCards(Arrays.asList(((CardsDatabase)CardsDatabase.getInstance()).queryByName("Tierno"),
                 new EnergyCard("Water","water"), new EnergyCard("Water","water"), new EnergyCard("Water", "water")));
-        Player player = new Player(deck);
+        Player player = Mockito.spy(new Player(deck));
 
         stub(method(GameController.class, "updateHand")).toReturn(0);
         stub(method(GameController.class, "getActivePlayer")).toReturn(player);
@@ -66,7 +68,8 @@ public class AbilityIT {
     public void testHealEffect(){
         Deck deck = new Deck();
         deck.addCards(Arrays.asList(((CardsDatabase)CardsDatabase.getInstance()).queryByName("Potion")));
-        Player player = new Player(deck);
+        Player player = Mockito.spy(new Player(deck));
+        TargetServiceHandler.getInstance().setYouPlayer(player);
 
         stub(method(GameController.class, "getIsHomePlayerPlaying")).toReturn(true);
         stub(method(GameController.class, "getHomePlayer")).toReturn(player);
