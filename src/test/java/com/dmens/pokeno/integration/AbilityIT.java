3c5ac7d8350d202282eae0713f963dcf8b559953
Devin Mens <devin.mens@gmail.com>
2017-07-31 10:15:38 -0400

    #117 Trying to get Integration tests to work after merge from master

--- src/test/java/com/dmens/pokeno/integration/AbilityIT.java
+++ src/test/java/com/dmens/pokeno/integration/AbilityIT.java
@@ -1,16 +1,12 @@
 package com.dmens.pokeno.integration;
+import static org.junit.Assert.*;
+import static org.powermock.api.support.membermodification.MemberMatcher.method;
+import static org.powermock.api.support.membermodification.MemberModifier.stub;
 
+import java.util.Arrays;
 
-import com.dmens.pokeno.card.EnergyCard;
-import com.dmens.pokeno.card.Pokemon;
-import com.dmens.pokeno.controller.GameController;
-import com.dmens.pokeno.database.AbilitiesDatabase;
-import com.dmens.pokeno.database.CardsDatabase;
-import com.dmens.pokeno.deck.Deck;
-import com.dmens.pokeno.player.Player;
 import com.dmens.pokeno.services.handlers.TargetServiceHandler;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,16 +16,29 @@ import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.util.Arrays;
+import com.dmens.pokeno.ability.Ability;
+import com.dmens.pokeno.card.EnergyCard;
+import com.dmens.pokeno.card.Pokemon;
+import com.dmens.pokeno.controller.GameController;
+import com.dmens.pokeno.database.AbilitiesDatabase;
+import com.dmens.pokeno.database.CardsDatabase;
+import com.dmens.pokeno.deck.Deck;
+import com.dmens.pokeno.effect.ApplyStatus;
+import com.dmens.pokeno.effect.Damage;
+import com.dmens.pokeno.effect.DrawCard;
+import com.dmens.pokeno.effect.Heal;
+import com.dmens.pokeno.player.Player;
 
-import static org.junit.Assert.assertEquals;
-import static org.powermock.api.support.membermodification.MemberMatcher.method;
-import static org.powermock.api.support.membermodification.MemberModifier.stub;
+/**
+ *
+ * @author James
+ */
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({GameController.class})
 @PowerMockIgnore("javax.management.*")
 public class AbilityIT {
+
     static String mAbilityName = "Ability";
     static String mEffectTarget = "opponent-active";
     static int mEffectValue = 20;
@@ -37,18 +46,14 @@ public class AbilityIT {
     static String mEffectStatus = "asleep";
     static String mEffectStatusDifferent = "poisoned";
 
-    private static final Logger LOG = LogManager.getLogger(AbilityIT.class);
-
     @Before
     public void setup(){
-        LOG.info("Setting up database instances");
         AbilitiesDatabase.getInstance().initialize("abilities.txt");
         CardsDatabase.getInstance().initialize("cards.txt");
 
         //Mocks
         PowerMockito.mockStatic(GameController.class);
     }
-
 
     @Test
     public void testDrawEffect(){
@@ -71,12 +76,15 @@ public class AbilityIT {
 
     @Test
     public void testHealEffect(){
-        Player player = Mockito.spy(new Player());
+        Deck deck = new Deck();
+        deck.addCards(Arrays.asList(((CardsDatabase)CardsDatabase.getInstance()).queryByName("Potion")));
+        Player player = Mockito.spy(new Player(deck));
         TargetServiceHandler.getInstance().setYouPlayer(player);
 
         stub(method(GameController.class, "getIsHomePlayerPlaying")).toReturn(true);
         stub(method(GameController.class, "getHomePlayer")).toReturn(player);
         stub(method(Player.class, "createPokemonOptionPane")).toReturn(0);
+        stub(method(GameController.class, "getActivePlayer")).toReturn(player);
 
         // Set Froakie as the active pokemon
         player.setActivePokemon(new Pokemon("Froakie"));
@@ -87,8 +95,7 @@ public class AbilityIT {
         assertEquals(40, player.getActivePokemon().getDamage());
 
         // Draw Potion
-        player.getHand().addCard(Arrays.asList(((CardsDatabase)CardsDatabase.getInstance()).queryByName("Potion")).get(0));
-//        player.drawCardsFromDeck(1);
+        player.drawCardsFromDeck(1);
         assertEquals(1, player.getHand().size());
         assertEquals("Potion", player.getHand().getCards().get(0).getName());
 
@@ -99,4 +106,5 @@ public class AbilityIT {
         // Expect the damage taken by Froakie to be 10 (Potion heals 30 damage)
         assertEquals(10, player.getActivePokemon().getDamage());
     }
+
 }
