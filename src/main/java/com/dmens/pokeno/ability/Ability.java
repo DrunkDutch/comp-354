2ee56d5aa0e3349dfeb8440f303b12ca19d2277f
Devin Mens <devin.mens@gmail.com>
2017-07-31 20:29:40 -0400

    Revert "Revert "Deenergize""
    This reverts commit aa04062

--- src/main/java/com/dmens/pokeno/ability/Ability.java
+++ src/main/java/com/dmens/pokeno/ability/Ability.java
@@ -154,6 +154,24 @@ public class Ability {
     }
     
     /*
+     * Get the single Deenergize effect.
+     * Assumption is that there is only one.
+     * 
+     * @return		Effect cast as Deenergize.
+     */
+    public Deenergize getDeenergizeEffect()
+    {
+    	for (Effect effect: mEffects)
+    	{
+    	   if(effect.getClass() == Deenergize.class)
+    	   {
+    		   return (Deenergize) effect;
+    	   }
+    	}
+    	 return null;
+    }
+    
+    /*
      * (non-Javadoc)
      * @see java.lang.Object#toString()
      */
