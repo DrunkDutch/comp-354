1beb25bc4254221ca22c9b61926bb6af76c2fdee
Devin Mens <devin.mens@gmail.com>
2017-08-01 20:17:15 -0400

    Closes #121 Reworked the initial test case and added some extra cases

--- src/test/java/com/dmens/pokeno/regression/Regression121.java
+++ src/test/java/com/dmens/pokeno/regression/Regression121.java
@@ -1,17 +1,22 @@
 package com.dmens.pokeno.regression;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 import static org.powermock.api.support.membermodification.MemberMatcher.method;
 import static org.powermock.api.support.membermodification.MemberModifier.stub;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import com.dmens.pokeno.view.GameBoard;
 import org.easymock.EasyMock;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.powermock.api.easymock.PowerMock;
 import org.powermock.api.mockito.PowerMockito;
@@ -30,22 +35,19 @@ import com.dmens.pokeno.deck.Deck;
 import com.dmens.pokeno.effect.Heal;
 import com.dmens.pokeno.player.Player;
 
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({GameController.class})
 @PowerMockIgnore("javax.management.*")
 public class Regression121
 {
+
+	static GameBoard mockBoard = Mockito.mock(GameBoard.class);
 	@Before
 	public void setup(){
-		PowerMock.mockStatic(GameController.class);
+		GameController.setBoard(mockBoard);
 	}
 	
-	@Ignore
+	@Test
 	public void victoryTest()
 	{
 		
@@ -68,9 +70,68 @@ public class Regression121
 		//stub(method(GameController.class, "getActivePlayer")).toReturn(player);
 		player.drawCardsFromDeck(2);
 		player.startTurn();
-		//System.out.println(GameController.checkGameOver());
-		assert(GameController.checkGameOver());
+		assertTrue(GameController.checkGameOver());
 		//assert(true);
 	}
-		
+
+	@Test
+	public void notDeckedTest(){
+
+		//Test for decking out
+		stub(method(GameController.class, "updateDeck")).toReturn(0);
+		stub(method(GameController.class, "updateHand")).toReturn(0);
+		stub(method(GameController.class, "getIsHomePlayerPlaying")).toReturn(true);
+		stub(method(GameController.class, "displayMessage")).toReturn(0);
+		//PowerMockito.when(System.exit(0)).thenReturn("abc");
+
+		EnergyCard e1 = new EnergyCard("Water", "water");
+		EnergyCard e2 = new EnergyCard("Water", "water");
+		Deck deck = new Deck();
+		deck.addCards(Arrays.asList(e1, e2));
+
+		//System.out.println(GameController.checkGameOver());
+		//set player
+		//stub(method(Player.class, "getDeck")).toReturn(deck);
+		Player player = new Player(deck);//mock(Player.class);
+		//stub(method(GameController.class, "getActivePlayer")).toReturn(player);
+		player.startTurn();
+		assertFalse(GameController.checkGameOver());
+		//assert(true);
+	}
+
+	@Test
+	public void rewardVictoryTest() {        //Test for decking out
+		stub(method(GameController.class, "updateDeck")).toReturn(0);
+		stub(method(GameController.class, "updateHand")).toReturn(0);
+		stub(method(GameController.class, "getIsHomePlayerPlaying")).toReturn(true);
+		stub(method(GameController.class, "displayMessage")).toReturn(0);
+		//PowerMockito.when(System.exit(0)).thenReturn("abc");
+
+		EnergyCard e1 = new EnergyCard("Water", "water");
+		EnergyCard e2 = new EnergyCard("Water", "water");
+		EnergyCard e3 = new EnergyCard("Water", "water");
+		EnergyCard e4 = new EnergyCard("Water", "water");
+		EnergyCard e5 = new EnergyCard("Water", "water");
+		EnergyCard e6 = new EnergyCard("Water", "water");
+		EnergyCard e7 = new EnergyCard("Water", "water");
+		Deck deck = new Deck();
+		deck.addCards(Arrays.asList(e1, e2, e3,e4,e5,e6, e7));
+
+		Pokemon p1 = mock(Pokemon.class);
+
+
+		Player player = new Player(deck);//mock(Player.class);
+
+		Player opponent = spy(new Player());
+		player.setOpponent(opponent);
+		opponent.benchPokemon(p1);
+
+		player.setUpRewards();
+		for (int i = 0; i < 6; i++){
+			player.collectPrize(0);
+		}
+		assertTrue(GameController.checkGameOver());
+
+
+	}
 }
